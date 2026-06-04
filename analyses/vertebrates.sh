500d2490dbfdc7d790fccb8a5c7f9c51e90a86a6
John Huelsenbeck <johnh@berkeley.edu>
2026-06-04 15:04:05 +0200

    script

---
+++ analyses/vertebrates.sh
@@ -0,0 +1,32 @@
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.atpase6.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.atpase6.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.atpase6.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.atpase6.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.atpase8.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.atpase8.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.atpase8.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.atpase8.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coi.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coi.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coi.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coi.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coii.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coii.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coii.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coii.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coiii.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coiii.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.coiii.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.coiii.tw2 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd1.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd1.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd1.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd1.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd2.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd2.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd2.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd2.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd3.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd3.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd3.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd3.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd4.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd4.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd4.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd4.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd4l.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd4l.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd4l.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd4l.tw5 -n 1000000 -p 100000 -w 5
+
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd6.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd6.tw0 -n 1000000 -p 100000 -w 0
+./small_tree -i /Users/johnh/GitHub/empirical_bayes_emma/analyses/nexus/vertebrates.nd6.nex -o /Users/johnh/GitHub/empirical_bayes_emma/analyses/mcmc_res/vert.nd6.tw5 -n 1000000 -p 100000 -w 5
